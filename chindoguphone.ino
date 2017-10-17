631732b6e9d8b1f65367d595df1f3d2f1c150ed9
James Linnegar <jlinnega@thoughtworks.com>
2017-10-17 17:33:36 +0100

    first commit

---
+++ chindoguphone.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  while (!Serial);
+  Serial.begin(115200);
+  Serial.println(F("Initializing....Phone"));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
